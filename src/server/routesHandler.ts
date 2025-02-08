cedd161cfad197ed1c2df39de31945c7cb3e7c3d
Dennis Wiechmann <penning.dennis@googlemail.com>
2025-02-08 10:00:38 +0100

    improved file based route handling

--- src/server/routesHandler.ts
+++ src/server/routesHandler.ts
@@ -1,21 +1,23 @@
 import { serveDir, serveFile } from "jsr:@std/http/file-server";
 import path from "node:path";
 
-const STATIC_FOLDER = 'static'
-const BASE_FOLDER = 'routes'
-const dirname = import.meta.dirname || ''
+const rootDir = Deno.cwd()
+const STATIC_FOLDER_NAME = 'static'
+const BASE_FOLDER_NAME = 'routes'
+
+const STATIC_FOLDER_DIR = path.join(rootDir, STATIC_FOLDER_NAME)
+const FILE_DIR = path.join(rootDir, 'src', 'server', BASE_FOLDER_NAME)
 
 
 export async function handleRoutes(req: Request): Promise<Response> {
   const fileRoute = await createFileRoute(req)
   
   if(fileRoute.isStaticRoute) {
-    console.log(fileRoute.staticDirectory)
-    return serveDir(req, { fsRoot: fileRoute.staticDirectory })
+    return serveDir(req, { fsRoot: STATIC_FOLDER_DIR })
   }
 
   if(!fileRoute.isValid) {
-    const file = await Deno.readTextFile(path.join(dirname, 'routes','404.html'))
+    const file = await Deno.readTextFile(path.join(FILE_DIR ,'404.html'))
     return new Response(file, {status: 404, headers: {'content-type': 'text/html'}})
   }
 
@@ -27,27 +29,22 @@ type FileRoute = {
   isStaticRoute: boolean,
   isValid: boolean,
   filePath: string,
-  staticDirectory: string
 }
 
-async function createFileRoute(req: Request): Promise<FileRoute> {
-  const staticDirectory = path.join(dirname, '..', '..')
-  const baseDirtectory = path.join(dirname, BASE_FOLDER)
-  
+async function createFileRoute(req: Request): Promise<FileRoute> { 
   const url = new URL(req.url)
-  const isStaticRoute = url.pathname.split('/')[1] === (STATIC_FOLDER)
+  const isStaticRoute = url.pathname.split('/')[1] === (STATIC_FOLDER_NAME)
 
   const isDirectory = url.pathname.endsWith('/');
   
   const filePath = isDirectory 
-    ? path.join(baseDirtectory, url.pathname, 'index.html') 
-    : path.join(baseDirtectory, url.pathname);
+    ? path.join(FILE_DIR, url.pathname, 'index.html') 
+    : path.join(FILE_DIR, url.pathname);
 
   return {
     isStaticRoute,
     isValid: await isFile(filePath),
     filePath,
-    staticDirectory,
   };
 }
 
